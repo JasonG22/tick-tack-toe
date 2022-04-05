d9573cf77f589a6ca5ae593b08218e9e6d757104
JasonG22 <jase.gillett@gmail.com>
2022-04-05 09:21:03 +0100

    Update script.js
    Created writeBoardToPage to loop board.squares array

--- js/script.js
+++ js/script.js
@@ -10,15 +10,26 @@ const gameBoard = (function () {
             square.setAttribute('class', 'squares');
             board.sqaures.push(square);
         }
+        return;
     } 
     const createArray = () => {
         _createSquares(board);
-        console.log(board);
+        console.log(board.sqaures);
+        return;
+    }
+    const writeBoardToPage = (board) => {
+        for(let i = 0; i < board.sqaures.length; i++) {
+            console.log(board.sqaures[i]);
+        }
+        return;
     }
     return {
+        board: board,
         createArray: createArray,
+        writeBoardToPage: writeBoardToPage,
     }
 })();
 
 gameBoard.createArray();
+gameBoard.writeBoardToPage(gameBoard.board);
 
