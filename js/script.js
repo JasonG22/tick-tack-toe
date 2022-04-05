940d82c86797fd9a8c526af35e3e7ca5a89381ca
JasonG22 <jase.gillett@gmail.com>
2022-04-05 10:45:24 +0100

    Update script.js
    Added _createSquares func for create td's and send to array

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@ const gameBoard = (function () {
     const board = {
         sqaures: [],
     }
-    
+     _createSquares = (board) => {
+        for(let i = 0; i < 9; i++) {
+            let square = document.createElement('td');
+            let index = [i];
+            square.setAttribute('id', index);
+            board.sqaures.push(square);
+            
+        }
+        return console.log(board.sqaures)
+     }
     return {
-        
+        board: board,
+        _createSquares: _createSquares,
     }
 })();
 
-_createBoard();
+gameBoard._createSquares(gameBoard.board);
