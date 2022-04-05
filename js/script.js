a4f556b32f44e3dc2c5bd9e72a457eb097d75f75
JasonG22 <jase.gillett@gmail.com>
2022-04-05 11:02:10 +0100

    Update script.js
    Removed all code from script.js gameBoard func

--- js/script.js
+++ js/script.js
@@ -2,20 +2,7 @@ const gameBoard = (function () {
     const board = {
         sqaures: [],
     }
-     _createSquares = (board) => {
-        for(let i = 0; i < 9; i++) {
-            let square = document.createElement('td');
-            let index = [i];
-            square.setAttribute('id', index);
-            board.sqaures.push(square);
-            
-        }
-        return console.log(board.sqaures)
-     }
-    return {
-        board: board,
-        _createSquares: _createSquares,
-    }
+    
 })();
 
 gameBoard._createSquares(gameBoard.board);
