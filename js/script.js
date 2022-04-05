03565c6d456e4642e8e3a8280f8e77bbf4a08723
JasonG22 <jase.gillett@gmail.com>
2022-04-05 08:52:09 +0100

    Update script.js
    Created _createsSquares for loop to create square div's

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@ const gameBoard = (function () {
     const board = {
         sqaures: [],
     }
+    const _createSquares = (board) => {
+        for(let i = 0; i < 9; i++) {
+            board.sqaures[i] = document.createElement('div');
+        }
+    } 
+    return {
+        _createSquares: _createSquares,
+        board: board,
+    }
 })();
