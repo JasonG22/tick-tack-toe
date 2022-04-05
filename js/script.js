a374c38bfddb8aab1829f592cf4c16c318482eaf
JasonG22 <jase.gillett@gmail.com>
2022-04-05 10:30:01 +0100

    Update script.js
    Removed createBoard func

--- js/script.js
+++ js/script.js
@@ -2,33 +2,7 @@ const gameBoard = (function () {
     const board = {
         sqaures: [],
     }
-    _createBoard = () => {
-        const gameTable = document.getElementById('gameTable');
-        const row1 = gameTable.insertRow();
-        const row2 = gameTable.insertRow();
-        const row3 = gameTable.insertRow();
-        for(let i = 1; i <= 3; i++) {
-            let square = row1.insertCell();
-            let index = [i];
-            square.setAttribute('id', index);
-            square.setAttribute('class', 'squares');
-            console.log(square);
-        }
-        for(let i = 4; i <= 6; i++) {
-            let square = row2.insertCell();
-            let index = [i];
-            square.setAttribute('id', index);
-            square.setAttribute('class', 'squares');
-            console.log(square);
-        }
-        for(let i = 7; i <= 9; i++) {
-            let square = row3.insertCell();
-            let index = [i];
-            square.setAttribute('id', index);
-            square.setAttribute('class', 'squares');
-            console.log(square);
-        }
-        return;
+    å
     }
     return {
         _createBoard: _createBoard,
