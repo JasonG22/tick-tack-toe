7433d25e43d99ef6f4fe1006b08b7229eda7d04b
JasonG22 <jase.gillett@gmail.com>
2022-04-05 10:15:43 +0100

    Updated script.js
    Removed _createSquares createArray and writeBoardToPage

--- js/script.js
+++ js/script.js
@@ -2,34 +2,10 @@ const gameBoard = (function () {
     const board = {
         sqaures: [],
     }
-    const _createSquares = (board) => {
-        for(let i = 1; i <= 9; i++) {
-            let square = document.createElement('div');
-            let index = [i];
-            square.setAttribute('id', index);
-            square.setAttribute('class', 'squares');
-            board.sqaures.push(square);
-        }
-        return;
-    } 
-    const createArray = () => {
-        _createSquares(board);
-        console.log(board.sqaures);
-        return;
-    }
-    const writeBoardToPage = (board) => {
-        for(let i = 0; i < board.sqaures.length; i++) {
-            console.log(board.sqaures[i]);
-        }
-        return;
-    }
+    
     return {
-        board: board,
-        createArray: createArray,
-        writeBoardToPage: writeBoardToPage,
+    
     }
 })();
 
-gameBoard.createArray();
-gameBoard.writeBoardToPage(gameBoard.board);
 
