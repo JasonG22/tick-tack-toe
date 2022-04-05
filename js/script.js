21863591c7ce0d7f0c64fd40b948611509687989
JasonG22 <jase.gillett@gmail.com>
2022-04-05 15:38:35 +0100

    Update script.js
    Added cellClick func to log cell value

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 const gameBoard = (function () {
     const board = {
         squares: [],
+
     }
-    
+    const cellClick = () => {
+        const cells = document.querySelectorAll(".squares");
+        cells.forEach(cell => {
+        cell.addEventListener('click', () => {
+        const position = cell.getAttribute('value');
+        //Change this to players choise
+        cell.textContent = 'X';
+        return position;
+    })
+    });
+}
     return {
-    
+        cellClick: cellClick,
     }
     
 })();
-gameBoard._createSquares(gameBoard.board)
-gameBoard.createBoard(gameBoard.board);
+gameBoard.cellClick();
